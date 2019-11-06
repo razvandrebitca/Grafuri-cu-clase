96319c45aee27b021332c6a7428b657b2c67f0c4
razvandrebitca <50980318+razvandrebitca@users.noreply.github.com>
2019-11-06 11:20:18 +0200

    Add files via upload

---
+++ Grafph with classes/Grafph with classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Grafph_with_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         
+
+            using (TextReader tr = File.OpenText(@"C:\Users\elev\AppData\Local\Temp\~vsABC2.txt"))
+            {
+                int n = int.Parse(tr.ReadLine());
+                int[,] a = new int[3,3];
+           for(int i = 1; i <= n; i++) { 
+                    for(int j=1;j<=n;j++)
+                
+                    a[i,j]=int.Parse(tr.ReadLine());
+                }
+                for (int i = 1; i <= n; i++) {
+                    for (int j = 1; j <= n; j++)
+                        Console.WriteLine(a[i,j]+" ");
+                    Console.WriteLine();
+                }
+            }
+           
+
+                    Console.ReadKey();
+ }
+    }
+}
